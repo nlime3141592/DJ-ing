e6ef87882b12ffbc3aa4c847c65d0098da89b009
YellowGreenLime <nlime3141592@naver.com>
2025-06-17 02:43:22 +0900

    [Feature] 볼륨을 이펙터와 같은 방식으로 처리

---
+++ DotNetDJ/DJ_Console/AudioFilter/VOLUME.cs
@@ -0,0 +1,39 @@
+﻿using NAudio.Wave;
+using System;
+
+namespace nl.AudioFilter
+{
+    public class VOLUME : IAudioFilter
+    {
+        // if true, then, mute volume.
+        public bool Bypass { get; set; } = false;
+
+        public float Volume
+        {
+            get => _b0;
+            set => _b0 = value;
+        }
+
+        #region Filter Coefficients
+        private float _b0;
+        #endregion
+
+        public VOLUME(float volume)
+        {
+            _b0 = volume;
+        }
+
+        public float Process(float sample)
+        {
+            if (Bypass)
+                return 0.0f;
+
+            return sample * _b0;
+        }
+
+        public void OnSourceChanged(ISampleProvider source)
+        {
+            // This block is intentionally no operation.
+        }
+    }
+}
