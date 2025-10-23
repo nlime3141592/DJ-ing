ced89b301be6660ce2531364995cc2af97d28b5b
limeLimelimeLime <nlime3141592@naver.com>
2025-10-23 18:01:24 +0900

    [Feature] 하드웨어 테스트용 회로 완성

--- DJ_Keyboard/DJ_Keyboard.ino
+++ DJ_Keyboard/DJ_Keyboard.ino
@@ -1,20 +1,52 @@
 #include <Keyboard.h>
 
-int rd0 = 0;
+#define PIN_SHLD 7
+#define PIN_Q 5
+#define PIN_CLK 3
 
-int blink = 0;
+#define PIN_S0 14
+#define PIN_S1 15
+#define PIN_S2 16
 
 uint8_t buffer0[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
 uint8_t buffer1[8] = { 0, 1, 0, 0, 0, 0, 0, 0 };
 uint8_t buffer2[8] = { 0, 2, 0, 0, 0, 0, 0, 0 };
 uint8_t buffer3[8] = { 0, 3, 0, 0, 0, 0, 0, 0 };
 
-void update_modifier(int lShift01, int rShift01)
+uint8_t keyset[][8] = {
+  { 1, 2, 3, 4, 5, 6, 7, 8 },
+  { 9, 10, 11, 12, 13, 14, 15, 16 },
+  { 17, 18, 19, 20, 21, 22, 23, 24 },
+  { 25, 26, 27, 28, 29, 30, 31, 32 },
+  { 33, 34, 35, 36, 37, 38, 39, 40 },
+  { 41, 42, 43, 44, 45, 46, 47, 48 }
+};
+
+void setaddr(int addr)
+{
+  digitalWrite(PIN_S0, addr & 1);
+  digitalWrite(PIN_S1, addr & 2);
+  digitalWrite(PIN_S2, addr & 4);
+}
+
+int read_matrix()
+{
+  digitalWrite(PIN_CLK, 0);
+  delayMicroseconds(5);
+  digitalWrite(PIN_CLK, 1);
+  delayMicroseconds(5);
+  return digitalRead(PIN_Q);
+}
+
+void update_modifier(int channel)
 {
   uint8_t flag = 0;
 
-  flag |= (lShift01 << 6);
-  flag |= (rShift01 << 2);
+  for (int i = 0; i < 8; ++i)
+  {
+    flag <<= 1;
+    flag |= read_matrix();
+  }
 
   buffer0[0] = flag;
   buffer1[0] = flag;
@@ -22,13 +54,55 @@ void update_modifier(int lShift01, int rShift01)
   buffer3[0] = flag;
 }
 
+void update_channel(int channel)
+{
+  // Load shift register.
+  setaddr(channel);
+  digitalWrite(PIN_SHLD, 0);
+  delayMicroseconds(5);
+  digitalWrite(PIN_CLK, 0);
+  delayMicroseconds(5);
+  digitalWrite(PIN_CLK, 1);
+  delayMicroseconds(5); // Load end here.
+  digitalWrite(PIN_SHLD, 1);
+  delayMicroseconds(5);
+
+  for (int i = 0; i < 8; ++i)
+  {
+    if (!read_matrix())
+    {
+      continue;
+    }
+    else if (buffer0[channel] == 0)
+    {
+      buffer0[channel] = keyset[channel][i];
+    }
+    else
+    {
+      buffer0[channel] = 0x00;
+      return;
+    }
+  }
+}
+
 void setup()
 {
-  pinMode(10, INPUT);
-  pinMode(A3, INPUT);
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+
+  pinMode(PIN_SHLD, INPUT);
+  pinMode(PIN_CLK, OUTPUT);
+  pinMode(PIN_Q, INPUT);
+
+  pinMode(PIN_S0, OUTPUT);
+  pinMode(PIN_S1, OUTPUT);
+  pinMode(PIN_S2, OUTPUT);
 
   pinMode(14, INPUT);
   pinMode(15, INPUT);
+
+  digitalWrite(PIN_CLK, 1);
+  digitalWrite(PIN_SHLD, 1);
 
   Keyboard.begin();
 }
@@ -37,30 +111,17 @@ void loop()
 {
   delay(10);
 
-  int rd1 = digitalRead(10);
-  blink = 1 - blink;
+  update_channel(0);
+  update_channel(1);
+  update_channel(2);
+  update_channel(3);
+  update_modifier(4);
 
-  update_modifier(digitalRead(14), digitalRead(15));
-
-  if (!rd0 && rd1) // On Key Press
-  {
-    buffer0[5] = 'a';
-  }
-  else if (rd0 && !rd1) // On Key Release
-  {
-    buffer0[5] = 0;
-  }
-
-  buffer1[2] = 254 + blink;
-  buffer2[3] = 254 + blink;
-  buffer3[4] = 254 + blink;
-  
-  buffer2[4] = analogRead(A3) >> 2;
+  buffer1[2] = analogRead(A0) >> 2;
+  buffer2[2] = analogRead(A1) >> 2;
 
   Keyboard.sendReport((KeyReport*)(buffer0));
   Keyboard.sendReport((KeyReport*)(buffer1));
   Keyboard.sendReport((KeyReport*)(buffer2));
   Keyboard.sendReport((KeyReport*)(buffer3));
-
-  rd0 = rd1;
 }
