186f1cf37dae3f03825cbacb615063bc264c18b5
YellowGreenLime <nlime3141592@naver.com>
2025-11-22 02:15:10 +0900

    [Test] 음원 메타데이터 파일 생성/읽기/쓰기 테스트 완료

--- DJSW/DJSW/Main.cpp
+++ DJSW/DJSW/Main.cpp
@@ -3,6 +3,7 @@
 #include <assert.h>
 #include "LoopBase.h"
 
+#include "djsw_file_metadata.h"
 #include "djsw_input_hid.h"
 #include "djsw_input_hid_controls.h"
 
@@ -22,9 +23,37 @@ int WINAPI WinMain(
 
     LoopInit(hInstance, hPrevInstance, lpCmdLine, nCmdShow);
 
+    // if bypass true, then, hid loop doesn't push input event.
+    hidMessageQueues[DJSW_HID_MESSAGE_QUEUE_IDX_DEBUG].bypass = false;
+
+    djWavMetaFile metafile = djWavMetaFile();
+
     while (LoopUpdate(hInstance, hPrevInstance, lpCmdLine, nCmdShow))
     {
         // Debug logic here.
+        HidMessage hidmsg;
+
+        while (hidMessageQueues[DJSW_HID_MESSAGE_QUEUE_IDX_DEBUG].Pop(&hidmsg))
+        {
+            switch (hidmsg.hidKey)
+            {
+            case DJSW_HID_SPLIT1:
+                if (hidmsg.message == DJSW_HID_MASK_MESSAGE_KEY_DOWN)
+                {
+                    metafile.Open(L"C:\\Test\\meta.djmeta");
+                    OutputDebugStringW(L"File Opened.\n");
+                }
+                break;
+            case DJSW_HID_SPLIT2:
+                if (hidmsg.message == DJSW_HID_MASK_MESSAGE_KEY_DOWN)
+                {
+                    metafile.SetHotCueIndex(2, 3456);
+                    metafile.Save();
+                    metafile.Close();
+                    OutputDebugStringW(L"File Closed.\n");
+                }
+            }
+        }
     }
     
     LoopFinal(hInstance, hPrevInstance, lpCmdLine, nCmdShow);
